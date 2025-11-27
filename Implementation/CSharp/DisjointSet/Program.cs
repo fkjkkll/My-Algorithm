59ee94408d92209c19328b4469063aeffa86473d
fkjkkll <626409368@qq.com>
2025-11-27 18:01:25 +0800

    优先队列和并查集

---
+++ Implementation/CSharp/DisjointSet/Program.cs
@@ -0,0 +1,92 @@
+﻿DisjointSet<string> ds = new(["合肥","厦门","重庆","北京","上海"]);
+ds.Union("北京", "上海");
+Console.WriteLine(ds.IsUnion("北京","厦门"));
+ds.Union("上海", "合肥");
+Console.WriteLine(ds.IsUnion("北京", "厦门"));
+ds.Union("厦门", "合肥");
+Console.WriteLine(ds.IsUnion("北京", "厦门"));
+
+Console.WriteLine("");
+
+public class DisjointSet<T> where T : notnull
+{
+    private Dictionary<T, int> _elementToIndex = new();
+    private List<int> _parents = new();
+    public int SetCount => _parents.Count(e => e < 0);
+
+    public DisjointSet(IEnumerable<T> datas)
+    {
+        foreach (var data in datas)
+        {
+            var uid = _parents.Count;
+            _parents.Add(-1);
+            _elementToIndex[data] = uid;
+        }
+    }
+
+    private int GetIndex(T element)
+    {
+        if (!_elementToIndex.TryGetValue(element, out var index))
+            throw new ArgumentException($"Element {element} not found in disjoint set");
+        return index;
+    }
+
+    private int Find(int index)
+    {
+        if (_parents[index] < 0)
+            return index;
+        return _parents[index] = Find(_parents[index]);
+    }
+
+    public bool IsUnion(T left, T right)
+    {
+        return Find(GetIndex(left)) == Find(GetIndex(right));
+    }
+
+    public void Union(T left, T right)
+    {
+        UnionByTreeHeight(left, right);
+        //UnionByTreeScale(left, right);
+    }
+
+    // 按秩归并方法一：比较树高
+    private void UnionByTreeHeight(T left, T right)
+    {
+        var leftRoot = Find(GetIndex(left));
+        var rightRoot = Find(GetIndex(right));
+        if (leftRoot == rightRoot)
+            return;
+        // 因为是负数，所以反过来
+        if (_parents[leftRoot] < _parents[rightRoot])
+        {
+            _parents[leftRoot] += _parents[rightRoot];
+            _parents[rightRoot] = leftRoot;
+        }
+        else
+        {
+            _parents[rightRoot] += _parents[leftRoot];
+            _parents[leftRoot] = rightRoot;
+        }
+    }
+
+    // 按秩归并方法二：比较树规模（个数）
+    private void UnionByTreeScale(T left, T right)
+    {
+        var leftRoot = Find(GetIndex(left));
+        var rightRoot = Find(GetIndex(right));
+        if (leftRoot == rightRoot)
+            return;
+        // 因为是负数，所以反过来
+        if (_parents[leftRoot] < _parents[rightRoot])
+        {
+            _parents[rightRoot] = leftRoot;
+        }
+        else
+        {
+            // 因为是负数，所以--
+            if (_parents[leftRoot] == _parents[rightRoot])
+                --_parents[rightRoot];
+            _parents[leftRoot] = rightRoot;
+        }
+    }
+}
--- Implementation/CSharp/PriorityQueue/Program.cs
+++ Implementation/CSharp/PriorityQueue/Program.cs
@@ -1,2 +1,69 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿PriorityQueue pq = new(new int[] { 34, 53, 21, 12, 5, 76, 95, 4, 32, 45 });
+while (!pq.IsEmpty)
+    Console.WriteLine(pq.Pop());
+
+public class PriorityQueue
+{
+    private List<int> _list;
+
+    public bool IsEmpty => _list.Count == 0;
+
+    public PriorityQueue() { _list = new(); }
+
+    public PriorityQueue(int[] arr)
+    {
+        // O(N)
+        //_list = arr.ToList();
+        //for (int i = (_list.Count - 1) >> 1; i >= 0; --i)
+        //    Down(i);
+
+        // O(Nlog(N))
+        _list = new();
+        foreach (var n in arr)
+            Push(n);
+    }
+
+    public void Push(int value)
+    {
+        int child = _list.Count;
+        _list.Add(value);
+        while (child > 0)
+        {
+            var parent = (child - 1) >> 1;
+            // 当前父亲大于插入值，找到了
+            if (_list[parent] >= value)
+                break;
+            _list[child] = _list[parent];
+            child = parent;
+        }
+        _list[child] = value;
+    }
+
+    public int Pop()
+    {
+        if (_list.Count == 0)
+            throw new InvalidOperationException("IsEmpty!");
+        var ret = _list[0];
+        _list[0] = _list[^1];
+        _list.RemoveAt(_list.Count - 1);
+        if (!IsEmpty)
+            Down(0);
+        return ret;
+    }
+
+    private void Down(int parent)
+    {
+        var origin = _list[parent];
+        for (int child = (parent << 1) + 1; child < _list.Count; child = (parent << 1) + 1)
+        {
+            if (child + 1 < _list.Count && _list[child + 1] > _list[child])
+                ++child;
+            if (_list[child] > origin)
+                _list[parent] = _list[child];
+            else
+                break;
+            parent = child;
+        } 
+        _list[parent] = origin;
+    }
+}
