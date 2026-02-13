6173830af8b5d8d4c8a3435e4dcd657069c4652f
Lizq <626409368@qq.com>
2026-02-13 20:44:57 +0800

    其他文件

--- Implementation/CSharp/Exercise/Program.cs
+++ Implementation/CSharp/Exercise/Program.cs
@@ -1,1 +1,28 @@
-﻿
+
+
+using System.Runtime.InteropServices;
+
+Item[] items = new Item[3]
+{
+    new Item() { Id = 1, Num = 1 },
+    new Item() { Id = 2, Num = 2 },
+    new Item() { Id = 3, Num = 3 },
+};
+var span = items.AsSpan();
+
+var bSpan = MemoryMarshal.Cast<Item, int>(span);
+foreach (var n in bSpan)
+{
+    Console.WriteLine(n);
+}
+
+
+public struct Item
+{
+    public int Id;
+    public int Num;
+    public override string ToString()
+    {
+        return $"{Id} {Num}";
+    }
+}
